c363670e5d6081e93db90a7d50d2f1e2e7fbd24f
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2023-10-30 16:08:30 -0700

    pkey and fkey are not semantic types; now stored explicitly

--- rtb/data/table.py
+++ rtb/data/table.py
@@ -14,8 +14,6 @@ class SemanticType(Enum):
     TEXT = "text"
     IMAGE = "image"
     TIME = "time"
-    PRIMARY_KEY = "primary_key"
-    FOREIGN_KEY = "foreign_key"
 
 
 @dataclass
@@ -25,29 +23,23 @@ class Table:
     df: pd.DataFrame
     stypes: dict[str, SemanticType]  # column name -> semantic type
     fkeys: dict[str, str]  # column name -> table name
-    ctime_col: "creation_time"  # name of column storing creation time
+    pkey: str = "id"  # name of primary key column
+    ctime_col: str = "creation_time"  # name of column storing creation time
 
     def validate(self) -> bool
         r"""Validate the table.
 
         Check:
-        1. Columns of df match keys of stypes.
-        2. Foreign keys in stypes match keys of fkeys.
-        3. There is exactly one primary key.
-        4. ctime column exists and has stype of time.
-        5. Columns are unique.
+        - Columns of df match keys of stypes.
+        - pkey column exists and has unique values.
+        - ctime column exists and has stype of time.
+        - Columns are unique.
         """
 
         raise NotImplementedError
 
-    @property
-    def primary_key(self) -> str:
-        r"""Return the name of the primary key."""
-
-        raise NotImplementedError
-
     def save(self, path: str | os.PathLike) -> None:
-        r"""Saves the table to a parquet file. Stores stypes and fkeys as
+        r"""Saves the table to a parquet file. Stores other attributes as
         parquet metadata."""
 
         assert str(path).endswith(".parquet")
