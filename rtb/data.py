ef126210ceb6cb618e6e72f1ea4e566e82478032
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2023-10-29 22:41:23 -0700

    add rtb.nn and comment rtb.data

--- rtb/data.py
+++ rtb/data.py
@@ -43,7 +43,7 @@ class Table:
         1. Columns of df match keys of stypes.
         2. Foreign keys in stypes match keys of fkeys.
         3. There is exactly one primary key.
-        4. ctime column exists.
+        4. ctime column exists and has stype of time.
         5. Columns are unique.
         """
 
@@ -185,35 +185,48 @@ class Dataset:
         Args:
             root: root directory to store dataset.
             task_names: list of tasks to create.
+
+        The Dataset class exposes the following attributes:
+            db_splits: split name -> Database
+            task_splits: task name -> split name -> Task
         """
 
+        # download
         path = f"{root}/{self.name}/raw"
         if not Path(f"{path}/done").exists():
             self.download(path)
             Path(f"{path}/done").touch()
 
+        # process, standardize and split
         path = f"{root}/{name}/processed/db"
         if not Path(f"{path}/done").exists():
             db = self.standardize_db(self.process_db())
 
+            # save database splits independently
             db_splits = self.split_db(db)
             for split, db in db_splits.items():
                 db.save(f"{path}/{split}")
             Path(f"{path}/done").touch()
 
+        # load database splits
         self.db_splits = {
             split: Database.load(f"{path}/{split}")
             for split in ["train", "val", "test"]
         }
 
+        # create tasks for each split
         self.task_splits = defaultdict(dict)
         for task_name in task_names:
             for split in ["train", "val", "test"]:
                 path = f"{root}/{name}/processed/tasks/{task_name}/{split}"
+
+                # save task split independent of other splits and tasks
                 if not Path(f"{path}/done").exists():
                     task = self.task_fns[task_name](self.db_splits[split])
                     task.save(path)
                     Path(f"{path}/done").touch()
+
+                # load task split
                 self.task_splits[task_name][split] = Task.load(path)
 
 
@@ -244,9 +257,11 @@ class Dataset:
 
         assert sum(self.splits.values()) == 1.0
 
+        # get time stamps for splits
         self.val_split_time = db.time_of_split(splits["train"])
         self.test_split_time = db.time_of_split(splits["train"] + splits["val"])
 
+        # split the database
         db_train, db_val_test = db.split_at(self.val_split_time)
         db_val, db_test = val_test.split_at(self.test_split_time)
 
