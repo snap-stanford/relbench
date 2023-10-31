81f76fc1eda399c6e3826a2a66ba9106440619a0
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2023-10-31 09:43:14 -0700

    redesign table class

--- rtb/data/table.py
+++ rtb/data/table.py
@@ -21,20 +21,13 @@ class Table:
     r"""A table in a database."""
 
     df: pd.DataFrame
-    stypes: dict[str, SemanticType]  # column name -> semantic type
+    pkey: str  # name of primary key column
     fkeys: dict[str, str]  # column name -> table name
-    pkey: str = "id"  # name of primary key column
-    ctime_col: str = "creation_time"  # name of column storing creation time
+    feat_cols: dict[str, SemanticType]  # column name -> semantic type
+    time_col: str | None = None  # name of column storing creation time
 
-    def validate(self) -> bool
-        r"""Validate the table.
-
-        Check:
-        - Columns of df match keys of stypes.
-        - pkey column exists and has unique values.
-        - ctime column exists and has stype of time.
-        - Columns are unique.
-        """
+    def validate(self) -> bool:
+        r"""Validate the table."""
 
         raise NotImplementedError
 
@@ -57,6 +50,3 @@ class Table:
         future (ctime > time_stamp) tables."""
 
         raise NotImplementedError
-
-
-
