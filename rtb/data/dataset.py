ce5095c4c0d82f08223811d22d9610f0c7b86e13
kexinhuang12345 <kexinh@stanford.edu>
2023-11-02 10:08:04 -0700

    add process_db & download function

--- rtb/data/dataset.py
+++ rtb/data/dataset.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 import os
 from pathlib import Path
+import boto3
+import json
 
 import rtb
 
@@ -33,7 +35,7 @@ class Dataset:
             db_splits: split name -> Database
             task_splits: task name -> split name -> Task
         """
-
+        self.root = root
         # download
         path = f"{root}/{self.name}/raw"
         if not Path(f"{path}/done").exists():
@@ -73,16 +75,91 @@ class Dataset:
                 self.task_splits[task_name][split] = Task.load(path)
 
     def download(self, path: str | os.PathLike) -> None:
-        r"""Downloads the raw data to the path directory. To be implemented by
-        subclass."""
+        """
+        Download a file from an S3 bucket.
 
-        raise NotImplementedError
+        Parameters:
+        - path (str): Local path where the file should be saved.
+
+        Returns:
+        None
+        """
+        
+        file_key = f"{self.root}/{self.name}"
+        bucket_name = 'XXX' ## TBD
+        region_name='us-west-2' ## TBD
+        
+        # Create an S3 client
+        s3 = boto3.client('s3', region_name=region_name)
+
+        # Download the file
+        s3.download_file(bucket_name, file_key, path)
+
 
     def process_db(self) -> rtb.data.database.Database:
         r"""Processes the raw data into a database. To be implemented by
         subclass."""
+        
+        '''
+        following this structure currently...
+        info.json:
+        
+        {
+            "dim_tables": ["customer", "brand", "product"],
+            "fact_tables": ["review"],
+            "schema": {
+                "customer": {
+                    "name": "text"
+                },
+                "product": {
+                    "brand": "text",
+                    "category": "text",
+                    "description": "text",
+                    "title": "text",
+                    "image": "image",
+                    "features": "text",
+                    "price": "numerical"
+                },
+                "review": {
+                    "creation_time": "timestamp",
+                    "customer_id": "foreign_key[reviewer]",
+                    "product_id": "foreign_key[product]",
+                    "rating": "numerical",
+                    "verified": "categorical",
+                    "review_text": "text",
+                    "summary": "text",
+                    "vote": "numerical"
+                }
+            }
+        }
+        
+        '''
+        
+        ## read info json file
+        info = json.read(f"{self.root}/{self.name}/processed/info.json")
+        
+        table2rtb_table = {}
+        
+        for table_name in info["schema"].keys():
+            df = pd.read_csv(os.path.join(f"{root}/{name}/processed/db", table_name, ".csv"))
+            if table_name in info["dim_tables"]:
+                pkey = table_name
+            else:
+                pkey = None
+            time_col = None
+            fkeys = []
+            feat_cols = []
+            for row_key, row_val in info["schema"][table_name].items():
+                if row_val == 'timestamp':
+                    time_col = row_key
+                elif 'foreign_key' in row_val:
+                    fkeys.append((row_key, row_val.split("[")[1].rstrip("]")))
+                else:
+                    feat_cols.append(row_key)
+            table2rtb_table[table_name] = rtb.data.table.Table(df, feat_cols, fkeys, pkey, time_col)
+                 
+        return rtb.data.database.Database(table2rtb_table)
 
-        raise NotImplementedError
 
     def standardize_db(
         self, db: rtb.data.database.Database
