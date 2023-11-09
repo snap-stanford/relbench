c6c5adfcf1d13df9622c386646ab23833ca7d55f
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2023-11-09 14:38:08 -0800

    fix repeated dataset processing bug

--- rtb/data/dataset.py
+++ rtb/data/dataset.py
@@ -40,6 +40,7 @@ class Dataset:
             # standardize() is common to all subclasses
 
             db.save(path)
+            Path(f"{path}/done").touch()
 
         # load database
         self._db = Database.load(path)
