e947fc8de39339a380b08a818835e035a2cd40c7
rusty1s <matthias.fey@tu-dortmund.de>
2023-11-21 14:40:37 +0000

    update

--- examples/train.py
+++ examples/train.py
@@ -36,7 +36,7 @@ parser.add_argument("--lr", type=float, default=0.01)
 parser.add_argument("--epochs", type=int, default=10)
 parser.add_argument("--batch_size", type=int, default=512)
 parser.add_argument("--channels", type=int, default=128)
-parser.add_argument("--num_neighbors", type=int, default=-1)
+parser.add_argument("--num_neighbors", type=int, default=5)
 parser.add_argument("--num_workers", type=int, default=6)
 args = parser.parse_args()
 
