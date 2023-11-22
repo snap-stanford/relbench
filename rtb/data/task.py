90e4e70ef38f262491803510dc5d08f52c55fc78
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2023-11-21 21:13:24 -0800

    refactor task tables

--- rtb/data/task.py
+++ rtb/data/task.py
@@ -6,8 +6,7 @@ from typing import List, Union
 import pandas as pd
 from typing_extensions import Self
 
-from rtb.data.database import Database
-from rtb.data.table import Table
+from rtb.data import Dataset
 
 
 class TaskType(Enum):
@@ -21,19 +20,94 @@ class TaskType(Enum):
 class Task:
     r"""A task on a dataset."""
 
+    min_time_col: str = "min_time"
+    max_time_col: str = "max_time"
+
     input_cols: List[str]
     target_col: str
     task_type: TaskType
-    window_sizes: List[int]
+    window_sizes: List[pd.Timedelta]
     metrics: List[str]
 
     def __init__(self, dataset: Dataset):
         self.dataset = dataset
 
-    def make_table(db: Database, time_window_df: pd.DataFrame) -> Table:
-        r"""To be implemented by subclass.
-
-        time_window_df should have columns window_min_time and window_max_time,
-        containing timestamps."""
+    def make_table(
+        db: Database, windows: List[Tuple[pd.Timestamp, pd.Timestamp]]
+    ) -> Table:
+        r"""To be implemented by subclass."""
 
         raise NotImplementedError
+
+    def train_table(
+        self,
+        window_size: Optional[pd.Timedelta] = None,
+        windows: Optional[Tuple[List[pd.Timestamp], List[pd.Timestamp]]] = None,
+    ) -> Table:
+        """Returns the train table for a task."""
+
+        assert window_size is None or windows is None
+
+        if windows is None:
+            if window_size is None:
+                window_size = self.window_sizes[0]
+
+            # default sampler
+            windows = rolling_window_sampler(
+                self.dataset.db_min_time,
+                self.dataset.train_max_time,
+                window_size,
+                stride=window_size,
+            )
+
+        # TODO: check inclusivity-exclusivity of windows
+        assert min(windows[0]) >= self.dataset.db_min_time
+        assert max(windows[1]) <= self.dataset.train_max_time
+
+        return self.make_table(self.dataset._db, windows)
+
+    def val_table(
+        self,
+        window_size: Optional[pd.Timedelta] = None,
+        windows: Optional[Tuple[List[pd.Timestamp], List[pd.Timestamp]]] = None,
+    ) -> Table:
+        r"""Returns the val table for a task."""
+
+        assert window_size is None or windows is None
+
+        if windows is None:
+            if window_size is None:
+                window_size = self.window_sizes[0]
+
+            # default sampler
+            windows = one_window_sampler(
+                self.dataset.train_max_time,
+                window_size,
+            )
+
+        assert min(windows[0]) >= self.dataset.train_max_time
+        assert max(windows[1]) <= self.dataset.val_max_time
+
+        return self.make_table(self.dataset._db, windows)
+
+    def test_table(
+        self,
+        window_size: Optional[pd.Timedelta] = None,
+    ) -> Table:
+        r"""Returns the test table for a task."""
+        if window_size is None:
+            window_size = self.window_sizes[0]
+
+        windows = one_window_sampler(
+            self.dataset.val_max_time,
+            window_size,
+        )
+        table = self.make_table(self.dataset._db, windows)
+
+        assert min(windows[0]) >= self.dataset.val_max_time
+        assert max(windows[1]) <= self.dataset.db_max_time
+
+        # only expose input columns to prevent info leakage
+        table.df = table.df[task.input_cols]
+
+        return table
