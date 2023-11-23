3e1c50dd315a29ef4dea15d80b536b690e129c00
Weihua Hu <weihua916@gmail.com>
2023-11-23 04:14:56 -0800

    Use Glove text embedding instead of transformer (#48)
    Embedding inference went down from 3 hours to 3 min.

--- examples/text_embedder.py
+++ examples/text_embedder.py
@@ -1,49 +1,17 @@
-from typing import List
+from typing import List, Optional
 
 import torch
-# Please run `pip install peft` to install the package
+# Please run `pip install -U sentence-transformers`
+from sentence_transformers import SentenceTransformer
 from torch import Tensor
-# Please run `pip install transformers` to install the package
-from transformers import AutoModel, AutoTokenizer
 
 
-def mean_pooling(last_hidden_state: Tensor, attention_mask) -> Tensor:
-    input_mask_expanded = (
-        attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
-    )
-    embedding = torch.sum(last_hidden_state * input_mask_expanded, dim=1) / torch.clamp(
-        input_mask_expanded.sum(1), min=1e-9
-    )
-    return embedding.unsqueeze(1)
+class GloveTextEmbedding:
 
-
-class TextToEmbedding:
-    def __init__(
-        self,
-        model: str = "sentence-transformers/all-distilroberta-v1",
-        pooling: str = "mean",
-        device: torch.device = torch.device("cpu"),
-    ):
-        self.tokenizer = AutoTokenizer.from_pretrained(model)
-        self.model = AutoModel.from_pretrained(model).to(device)
-        self.device = device
-        self.pooling = pooling
+    def __init__(self, device: Optional[torch.device] = None):
+        self.model = SentenceTransformer(
+            "sentence-transformers/average_word_embeddings_glove.6B.300d",
+            device=device)
 
     def __call__(self, sentences: List[str]) -> Tensor:
-        inputs = self.tokenizer(
-            sentences,
-            truncation=True,
-            padding="max_length",
-            return_tensors="pt",
-        )
-        for key in inputs:
-            if isinstance(inputs[key], Tensor):
-                inputs[key] = inputs[key].to(self.device)
-        out = self.model(**inputs)
-        mask = inputs["attention_mask"]
-        if self.pooling == "mean":
-            return mean_pooling(out.last_hidden_state.detach(), mask).squeeze(1).cpu()
-        elif self.pooling == "cls":
-            return out.last_hidden_state[:, 0, :].detach().cpu()
-        else:
-            raise ValueError(f"{self.pooling} is not supported.")
+        return torch.from_numpy(self.model.encode(sentences))
