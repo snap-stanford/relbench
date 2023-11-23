a231196a0bb17c6fedfdc3637cd5ed42c9f1a5e0
Kexin Huang <kexinh@stanford.edu>
2023-11-23 10:29:29 -0800

    add the question popularity task (#49)

--- rtb/datasets/forum_full.py
+++ rtb/datasets/forum_full.py
@@ -12,67 +12,14 @@ from rtb.data.task import Task, TaskType
 from rtb.utils import get_df_in_window, to_unix_time
 
 
-class UserNumPostsTask(Task):
-    r"""Predict the number of posts a user will make in the next 3 months."""
 
-    def __init__(self):
-        super().__init__(
-            input_cols=["window_min_time", "window_max_time", "OwnerUserId"],
-            target_col="num_posts",
-            task_type=TaskType.REGRESSION,
-            window_sizes=[pd.Timedelta(days=90)],
-            metrics=["mse", "smape"],
-        )
-
-    def make_table(self, db: Database, time_window_df: pd.DataFrame) -> Table:
-        r"""Create Task object for UserNumPosts."""
-
-        posts = db.tables["posts"].df
-        posts = posts[
-            posts.OwnerUserId != -1
-        ]  ## when user id is -1, it is stats exchange community, not a real person
-        posts = posts[posts.OwnerUserId.notnull()]  ## 1153 null posts
-        users = db.tables["users"].df
-
-        def get_values_in_window(row, posts, users):
-            posts_window = get_df_in_window(posts, "CreationDate", row)
-            users_exist = users[
-                users.CreationDate <= row["window_min_time"]
-            ]  ## only looking at existing users
-            users_exist_ids = users_exist.Id.values
-            user2num_posts = pd.DataFrame()
-            user2num_posts["OwnerUserId"] = users_exist_ids
-            user2num_posts["window_min_time"] = row["window_min_time"]
-            user2num_posts["window_max_time"] = row["window_max_time"]
-
-            new_posts = dict(posts_window.groupby("OwnerUserId").Id.agg(len))
-            user2num_posts["num_posts"] = user2num_posts.OwnerUserId.apply(
-                lambda x: new_posts[x] if x in new_posts else 0
-            )  ## default all existing users have 0 posts
-            return user2num_posts
-
-        # Apply function to each time window
-        res = time_window_df.apply(
-            lambda row: get_values_in_window(row, posts, users), axis=1
-        )
-        df = pd.concat(res.values)
-
-        return Table(
-            df=df,
-            fkey_col_to_pkey_table={"OwnerUserId": "users"},
-            pkey_col=None,
-            time_col="window_min_time",
-        )
-
-
-
-class UserChurnTask(Task):
+class UserContributionTask(Task):
     r"""Predict if a user will make any votes/posts/comments in the next 3 years."""
 
     def __init__(self):
         super().__init__(
             input_cols=["window_min_time", "window_max_time", "OwnerUserId"],
-            target_col="churn",
+            target_col="contribution",
             task_type=TaskType.BINARY_CLASSIFICATION,
             window_sizes=[pd.Timedelta(days=365*2)],
             metrics=["auroc", "auprc"],
@@ -88,7 +35,7 @@ class UserChurnTask(Task):
             posts.OwnerUserId != -1
         ]  ## when user id is -1, it is stats exchange community, not a real person
         posts = posts[posts.OwnerUserId.notnull()]  ## 1153 null posts
-        
+
         users = db.tables["users"].df
         votes = votes[votes.UserId.notnull()]  
         posts = posts[posts.OwnerUserId.notnull()]  
@@ -97,7 +44,7 @@ class UserChurnTask(Task):
             comments.UserId != -1
         ]  ## when user id is -1, it is stats exchange community, not a real person
         comments = comments[comments.UserId.notnull()]  ## 2439 null comments
-        
+
         def get_values_in_window(row, posts, users):
             posts_window = get_df_in_window(posts, "CreationDate", row)
             comments_window = get_df_in_window(comments, "CreationDate", row)
@@ -106,9 +53,9 @@ class UserChurnTask(Task):
             user_made_posts_in_this_period = posts_window.OwnerUserId.unique()
             user_made_comments_in_this_period = comments_window.UserId.unique()
             user_made_votes_in_this_period = votes_window.UserId.unique()
-            
+
             #user_active_in_this_period = user_made_posts_in_this_period
-            
+
             user_active_in_this_period = np.union1d(np.union1d(user_made_posts_in_this_period, user_made_comments_in_this_period),user_made_votes_in_this_period)
 
             users_exist = users[
@@ -122,15 +69,15 @@ class UserChurnTask(Task):
             active_user_before_this_time = np.union1d(np.union1d(user_made_votes, user_made_comments),user_made_posts)
             #active_user_before_this_time = user_made_posts
             users_exist_and_active_ids = np.intersect1d(users_exist_ids, active_user_before_this_time)
-            
+
             user2churn = pd.DataFrame()
             user2churn["OwnerUserId"] = users_exist_and_active_ids
             user2churn["window_min_time"] = row["window_min_time"]
             user2churn["window_max_time"] = row["window_max_time"]            
-            
+
             user2churn["churn"] = user2churn.OwnerUserId.apply(
-                lambda x: 0 if x in user_active_in_this_period else 1
-            )  ## 1: churn; 0: not churn
+                lambda x: 1 if x in user_active_in_this_period else 0
+            )  ## 1: contributed; 0: not contributed
             return user2churn
         tqdm.pandas()
         # Apply function to each time window
@@ -149,15 +96,15 @@ class UserChurnTask(Task):
 
 
 
-class PostUpvotesTask(Task):
-    r"""Predict the number of upvotes that a post will receive in the next week."""
+class QuestionPopularityTask(Task):
+    r"""Predict the number of upvotes that a question that is posted within the last 2 years will receive in the next 6 months. ?"""
 
     def __init__(self):
         super().__init__(
             input_cols=["window_min_time", "window_max_time", "PostId"],
-            target_col="num_upvotes",
+            target_col="popularity",
             task_type=TaskType.REGRESSION,
-            window_sizes=[pd.Timedelta("1W")],
+            window_sizes=[pd.Timedelta(days=180)],
             metrics=["mse", "smape"],
         )
 
@@ -174,9 +121,11 @@ class PostUpvotesTask(Task):
         ]  ## when user id is -1, it is stats exchange community, not a real person
         posts = posts[posts.OwnerUserId.notnull()]  ## 1153 null posts
 
+        posts = posts[posts.PostTypeId == 1] ## just looking at questions
+
         def get_values_in_window(row, votes, posts):
             votes_window = get_df_in_window(votes, "CreationDate", row)
-            posts_exist = posts[posts.CreationDate <= row["window_min_time"]]
+            posts_exist = posts[(posts.CreationDate <= row["window_min_time"]) & (posts.CreationDate > (row["window_min_time"] - pd.Timedelta(days = 365*2)))] ## posts exist and active defined by created in the last 2 years
             posts_exist_ids = posts_exist.Id.values
             train_table = pd.DataFrame()
             train_table["PostId"] = posts_exist_ids
@@ -184,13 +133,14 @@ class PostUpvotesTask(Task):
             train_table["window_max_time"] = row["window_max_time"]
 
             num_of_upvotes = dict(votes_window.groupby("PostId")["Id"].agg(len))
-            train_table["num_upvotes"] = train_table.PostId.apply(
+            train_table["popularity"] = train_table.PostId.apply(
                 lambda x: num_of_upvotes[x] if x in num_of_upvotes else 0
             )  ## default all existing users have 0 comment scores
             return train_table
 
+        tqdm.pandas()
         # Apply function to each row in df_b
-        res = time_window_df.apply(
+        res = time_window_df.progress_apply(
             lambda row: get_values_in_window(row, votes, posts), axis=1
         )
         df = pd.concat(res.values)
@@ -211,9 +161,8 @@ class ForumDataset(Dataset):
         ## needs to brainstorm a bit about meaningful tasks
 
         tasks = {
-            "PostUpvotesTask": PostUpvotesTask(),
-            "UserNumPostsTask": UserNumPostsTask(),
-            "UserChurnTask": UserChurnTask()
+            "QuestionPopularityTask": QuestionPopularityTask(),
+            "UserContributionTask": UserContributionTask()
         }
         self.tasks_window_size = {i: j.window_sizes[0] for i, j in tasks.items()}
         return tasks
@@ -229,7 +178,7 @@ class ForumDataset(Dataset):
         postLinks = pd.read_csv(os.path.join(path, "PostLinks.csv"))
         badges = pd.read_csv(os.path.join(path, "Badges.csv"))
         postHistory = pd.read_csv(os.path.join(path, "PostHistory.csv"))
-        
+
         # tags = pd.read_csv(os.path.join(path, "Tags.csv")) we remove tag table here since after removing time leakage columns, all information are kept in the posts tags columns
 
         ## remove time leakage columns
@@ -237,10 +186,10 @@ class ForumDataset(Dataset):
             columns=["Reputation", "Views", "UpVotes", "DownVotes", "LastAccessDate"], inplace=True
         )
         posts.drop(columns=["ViewCount", "AnswerCount", "CommentCount", "FavoriteCount", "CommunityOwnedDate", "ClosedDate", "LastEditDate", "LastActivityDate", "Score"], inplace=True)
-        
+
         comments.drop(columns=["Score"], inplace=True)
         votes.drop(columns=["BountyAmount"], inplace=True)
-                
+
         ## change time column to unix time
         comments["CreationDate"] = to_unix_time(comments["CreationDate"])
         badges["Date"] = to_unix_time(badges["Date"])
