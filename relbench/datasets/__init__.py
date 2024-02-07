dba2867c89d594a8a9128e37ceb7d0c1cd4575ae
Joshua Robinson <robinsonjd17@gmail.com>
2024-02-07 09:35:26 -0800

    remove more f1 files

--- relbench/datasets/__init__.py
+++ relbench/datasets/__init__.py
@@ -1,6 +1,5 @@
 from relbench.data import RelBenchDataset
 from relbench.datasets.amazon import AmazonDataset
-from relbench.datasets.f1 import F1Dataset
 from relbench.datasets.fake import FakeDataset
 from relbench.datasets.stackex import StackExDataset
 
