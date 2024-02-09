dddef11b3cbdb4e1713f1df8fa2ce3fa0d5ebe32
Joshua Robinson <robinsonjd17@gmail.com>
2024-02-09 12:32:38 -0800

    rm unwanted relgym code from this branch

--- run/run_single.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Test for running a single experiment. --repeat means run how many different random seeds.
-python run/main.py --cfg run/configs/example_test2.yaml --repeat 1 --auto_select_device
