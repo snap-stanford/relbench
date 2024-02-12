1926e02e1786739ddb6e6e5f25ac8f992604784f
Joshua Robinson <robinsonjd17@gmail.com>
2024-02-12 13:22:01 -0800

    add data download from website

--- relbench/datasets/f1.py
+++ relbench/datasets/f1.py
@@ -29,9 +29,15 @@ class F1Dataset(RelBenchDataset):
 
     def make_db(self) -> Database:
         r"""Process the raw files into a database."""
-        path = "relbench/datasets/f1-temp"  # temporary path for development
-
-        path = os.path.join(path, "raw")
+        url = "https://relbench.stanford.edu/data/relbench-f1-raw.zip"
+        path = pooch.retrieve(
+            url,
+            known_hash='5552653fc4f0442427c6299cae0615e4acb7677a9a07b29b87b0e93736ced061',
+            progressbar=True,
+            processor=unzip_processor,
+        )
+        
+        path = os.path.join(path, "f1-temp/raw")
 
         print("Current working directory:", os.getcwd())
 
