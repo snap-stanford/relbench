d34d5621c067a60ee82d90e10e4158103267a57c
Artur Lesniak <artur.lesniak@intel.com>
2024-03-06 11:25:27 +0000

    better check_db()

--- relbench/datasets/stackex.py
+++ relbench/datasets/stackex.py
@@ -18,6 +18,9 @@ from relbench.utils import unzip_processor
 
 class StackExDataset(RelBenchDataset):
     name = "rel-stackex"
+    url = "https://relbench.stanford.edu/data/relbench-forum-raw.zip"
+    known_hash = "ad3bf96f35146d50ef48fa198921685936c49b95c6b67a8a47de53e90036745f"
+    inf_file_line = f"{url} = {known_hash}"
     # 2 years gap
     val_timestamp = pd.Timestamp("2019-01-01")
     test_timestamp = pd.Timestamp("2021-01-01")
@@ -39,23 +42,17 @@ class StackExDataset(RelBenchDataset):
     ):
         self.name = f"{self.name}"
         self.local_db_cache_path = os.path.join(pooch.os_cache(self.name), self.db_dir, 'raw')
+        self.inf_file_path = os.path.join(self.local_db_cache_path, "db.inf")
         self.use_db_cache = use_db_cache
         self.keep_raw_csv = keep_raw_csv  # don't delete imtermediate csv files
 
         super().__init__(process=process)
 
-    def check_db(self) -> bool:
-        r"Check if local database is OK"
-        if os.path.isdir(self.local_db_cache_path):
-            return any(os.scandir(self.local_db_cache_path))  # check if empty
-        return False
-
     def create_db(self) -> Tuple[Database, str]:
         r"""Process the raw files into a database."""
-        url = "https://relbench.stanford.edu/data/relbench-forum-raw.zip"
         path = pooch.retrieve(
-            url,
-            known_hash="ad3bf96f35146d50ef48fa198921685936c49b95c6b67a8a47de53e90036745f",
+            self.url,
+            known_hash=self.known_hash,
             progressbar=True,
             processor=unzip_processor,
         )
@@ -171,25 +168,47 @@ class StackExDataset(RelBenchDataset):
 
         return Database(tables), path
 
+    def check_db(self) -> bool:
+        r"Check if local database is OK"
+        if Path(self.inf_file_path).exists():
+            with open(self.inf_file_path) as f:
+                # check if inf_file contains matching url and hash of zip file
+                return f.readline().strip() == self.inf_file_line
+        return False
+
+    def clear_local_db(self, db_path:str):
+        print("Removing db cache files")
+        for table_path in Path(db_path).glob("*.parquet"):
+            os.remove(str(table_path))
+        os.remove(os.path.join(db_path, "db.inf"))
+
+    def clear_csv(self, csv_path:str):
+        print("Removing raw csv files")
+        for table_path in Path(csv_path).glob("*.csv"):
+            os.remove(str(table_path))
+
     def make_db(self) -> Database:
         r"""Process the raw files into a database or load cached database."""
+        db_path = self.local_db_cache_path
         if self.use_db_cache and self.check_db():
-            print(f"Loading db from {self.local_db_cache_path}")
-            db = Database.load(self.local_db_cache_path)
+            print(f"Loading db from {db_path}")
+            db = Database.load(db_path)
         else:
-            db, path = self.create_db()
+            db, csv_path = self.create_db()
             if self.use_db_cache:
                 if not self.keep_raw_csv:
-                    # raw csv files are not needed when db_cache is used
-                    print("Removing raw csv files")
-                    for table_path in Path(path).glob("*.csv"):
-                        os.remove(table_path)
+                # raw csv files are not needed when db_cache is used
+                    self.clear_csv(csv_path)
 
-                db.save(self.local_db_cache_path)
+                print(f"saving Database object to {db_path}")
+                db.save(db_path)
+
+                # add db.inf file to the db_path with url and known_hash
+                with open(self.inf_file_path, 'w') as f:
+                    f.write(f"{self.inf_file_line}\n")
             elif self.check_db():
                 # local cache database files are not needed in this case
-                print("Removing db cache files")
-                for table_path in Path(self.local_db_cache_path).glob("*.parquet"):
-                    os.remove(table_path)
+                self.clear_local_db(db_path)
 
         return db
+
