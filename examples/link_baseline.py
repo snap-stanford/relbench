9f0d2d557ec3d4a09455fe5e494fafe2c507dfba
Joshua Robinson <robinsonjd17@gmail.com>
2024-03-08 13:50:06 -0800

    modifications to link baseline

--- examples/link_baseline.py
+++ examples/link_baseline.py
@@ -16,8 +16,9 @@ args = parser.parse_args()
 dataset = get_dataset(name=args.dataset, process=True)
 task = dataset.get_task(args.task, process=True)
 
-train = task.train_table.df
-test = task.test_table.df
+train_table = task.train_table
+test_table = task.test_table
+val_table = task.val_table
 PAD = -1
 
 
@@ -32,7 +33,7 @@ def count_and_sort(int_list):
     # Return the sorted list of unique ints by frequency
     return sorted_ints
 
-def pad_preds(max_len, preds):
+def count_sort_and_pad_preds(max_len, preds):
     padded_preds = []
 
     max_len = task.eval_k
@@ -49,33 +50,49 @@ def pad_preds(max_len, preds):
 # =============================================================================
 # predict the most frequent dst entity for each src entity
 # =============================================================================
-def most_frequent_dst_entity(train, test, task):
-    src_ids = list(task.test_table.df[task.src_entity_col])
+def most_frequent_dst_entity(train_table, test_table, val_table, task):
+    src_ids = list(task.train_table.df[task.src_entity_col])
+
+    src_to_dst = {}
+
+    for index, row in train_table.df.iterrows():
+        src = row[task.src_entity_col]
+        dst = row[task.dst_entity_col]
+        if src not in src_to_dst:
+            src_to_dst[src] = []
+        src_to_dst[src] += dst
+
+    for index, row in val_table.df.iterrows():
+        src = row[task.src_entity_col]
+        dst = row[task.dst_entity_col]
+        if src not in src_to_dst:
+            src_to_dst[src] = []
+        src_to_dst[src] += dst
+
 
     preds = []
-    for id in src_ids:
-        matching = list(train[train[task.src_entity_col] == id][task.dst_entity_col])
-        # Take the union of all lists
-        union = set().union(*matching)
-
-        # Convert the result back to a list 
-        pred = list(union)
+    for id in list(task.test_table.df[task.src_entity_col]):
+        if id in src_to_dst:
+            pred = src_to_dst[id]
+        else:
+            pred = [] # If the src entity is not in the train or val set, predict an empty list
 
         preds.append(pred)
-
-    padded_preds = pad_preds(task.eval_k, preds)
-    preds = np.array(padded_preds) 
-
+    
+    padded_preds = count_sort_and_pad_preds(task.eval_k, preds)
+    preds = np.array(padded_preds)
     return preds
+
 
 # =============================================================================
 # predict random dst entities for each src entity
 # =============================================================================
-def random_dst_entity(train, test, task):
+def random_dst_entity(train_table, test_table, val_table, task):
     src_ids = list(task.test_table.df[task.src_entity_col])
     max_len = task.eval_k
 
-    dst_list = [item for sublist in list(train[task.dst_entity_col]) for item in sublist]
+    dst_list = [item for sublist in list(train_table.df[task.dst_entity_col]) for item in sublist]
+    dst_list += [item for sublist in list(val_table.df[task.dst_entity_col]) for item in sublist]
     dst_list = list(set(dst_list)) # get unique ids
 
     preds = []
@@ -84,16 +101,16 @@ def random_dst_entity(train, test, task):
 
         preds.append(pred)
 
-    padded_preds = pad_preds(task.eval_k, preds)
+    padded_preds = count_sort_and_pad_preds(task.eval_k, preds)
     preds = np.array(padded_preds) 
 
     return preds
 
 for run in range(args.repeats):
     if args.method == "most_frequent":
-        preds = most_frequent_dst_entity(train, test, task)
+        preds = most_frequent_dst_entity(train_table, test_table, val_table, task)
     elif args.method == "random":
-        preds = random_dst_entity(train, test, task)
+        preds = random_dst_entity(train_table, test_table, val_table, task)
         
     print(f"Run: {run}")
     print(f"Method: {args.method}")
