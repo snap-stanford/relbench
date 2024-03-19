ff1f16112007237ac44177de499d067b02a783d7
Weihua Hu <weihua916@gmail.com>
2024-03-19 23:28:17 +0000

    update

--- examples/link_baseline.py
+++ examples/link_baseline.py
@@ -1,129 +1,87 @@
 import argparse
-import random
 from collections import Counter
+from typing import Dict
 
-import duckdb
 import numpy as np
 import pandas as pd
 
+from relbench.data import LinkTask, Table
 from relbench.datasets import get_dataset
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, default="rel-stackex")
 parser.add_argument("--task", type=str, default="rel-stackex-comment-on-post")
-parser.add_argument("--repeats", type=int, default=1)
-parser.add_argument("--method", type=str, default="most_frequent")
 args = parser.parse_args()
 
 dataset = get_dataset(name=args.dataset, process=True)
-task = dataset.get_task(args.task, process=True)
+task: LinkTask = dataset.get_task(args.task, process=True)
 
 train_table = task.train_table
 test_table = task.test_table
 val_table = task.val_table
-PAD = -1
+
+trainval_table_df = pd.concat([train_table.df, val_table.df], axis=0)
+trainval_table = Table(
+    df=trainval_table_df,
+    fkey_col_to_pkey_table=train_table.fkey_col_to_pkey_table,
+    pkey_col=train_table.pkey_col,
+    time_col=train_table.time_col,
+)
 
 
-# Count the frequency of each int in the list
-def count_and_sort(int_list):
-    # Count the frequency of each int
-    freq_counter = Counter(int_list)
-
-    # Sort the ints by frequency in descending order
-    sorted_ints = sorted(freq_counter, key=lambda x: freq_counter[x], reverse=True)
-
-    # Return the sorted list of unique ints by frequency
-    return sorted_ints
-
-
-def count_sort_and_pad_preds(max_len, preds):
-    padded_preds = []
-
-    max_len = task.eval_k
-    for p in preds:
-        p = count_and_sort(p)
-        if len(p) < max_len:
-            p = p + (max_len - len(p)) * [PAD]
-        else:
-            p = p[:max_len]
-        padded_preds.append(p)
-
-    return padded_preds
+def past_visit_aggr(x, k: int):
+    lst_cat = []
+    for e in list(x):
+        lst_cat.extend(e)
+    counter = Counter(lst_cat)
+    topk = [elem for elem, _ in counter.most_common(k)]
+    # padding
+    if len(topk) < k:
+        topk.extend([-1] * (k - len(topk)))
+    return topk
 
 
-# =============================================================================
-# predict the most frequent dst entity for each src entity
-# =============================================================================
-def most_frequent_dst_entity(train_table, test_table, val_table, task):
-    src_ids = list(task.train_table.df[task.src_entity_col])
-
-    src_to_dst = {}
-
-    for index, row in train_table.df.iterrows():
-        src = row[task.src_entity_col]
-        dst = row[task.dst_entity_col]
-        if src not in src_to_dst:
-            src_to_dst[src] = []
-        src_to_dst[src] += dst
-
-    for index, row in val_table.df.iterrows():
-        src = row[task.src_entity_col]
-        dst = row[task.dst_entity_col]
-        if src not in src_to_dst:
-            src_to_dst[src] = []
-        src_to_dst[src] += dst
-
-    preds = []
-    for id in list(task.test_table.df[task.src_entity_col]):
-        if id in src_to_dst:
-            pred = src_to_dst[id]
-        else:
-            pred = (
-                []
-            )  # If the src entity is not in the train or val set, predict an empty list
-
-        preds.append(pred)
-
-    padded_preds = count_sort_and_pad_preds(task.eval_k, preds)
-    preds = np.array(padded_preds)
-    return preds
+def evaluate(
+    train_table: Table,
+    pred_table: Table,
+    name: str,
+) -> Dict[str, float]:
+    is_test = task.dst_entity_col not in pred_table.df
+    if name == "past_visit":
+        """Predict the most frequently-visited dst nodes per each src node."""
+        df = (
+            train_table.df.groupby(task.src_entity_col)[task.dst_entity_col]
+            .apply(lambda x: past_visit_aggr(x, task.eval_k))
+            .reset_index(name="__pred__")
+        )
+        pred_ser = pd.merge(pred_table.df, df, how="left", on=task.src_entity_col)[
+            "__pred__"
+        ]
+        # Replace NaN with [-1, -1, ..., -1] prediction
+        pred_ser = pred_ser.apply(
+            lambda x: x if isinstance(x, list) else [-1] * task.eval_k
+        )
+        pred = np.stack(pred_ser.values)
+    elif name == "global_popularity":
+        """Predict the globally most visited dst nodes and predict them across
+        the src nodes."""
+        lst_cat = []
+        for lst in train_table.df[task.dst_entity_col]:
+            lst_cat.extend(lst)
+        counter = Counter(lst_cat)
+        topk = np.array([elem for elem, _ in counter.most_common(task.eval_k)])
+        pred = np.tile(topk, (len(pred_table), 1))
+    else:
+        raise ValueError("Unknown eval name called {name}.")
+    return task.evaluate(pred, None if is_test else pred_table)
 
 
-# =============================================================================
-# predict random dst entities for each src entity
-# =============================================================================
-def random_dst_entity(train_table, test_table, val_table, task):
-    src_ids = list(task.test_table.df[task.src_entity_col])
-    max_len = task.eval_k
-
-    dst_list = [
-        item
-        for sublist in list(train_table.df[task.dst_entity_col])
-        for item in sublist
-    ]
-    dst_list += [
-        item for sublist in list(val_table.df[task.dst_entity_col]) for item in sublist
-    ]
-    dst_list = list(set(dst_list))  # get unique ids
-
-    preds = []
-    for id in src_ids:
-        pred = random.sample(dst_list, max_len)
-
-        preds.append(pred)
-
-    padded_preds = count_sort_and_pad_preds(task.eval_k, preds)
-    preds = np.array(padded_preds)
-
-    return preds
-
-
-for run in range(args.repeats):
-    if args.method == "most_frequent":
-        preds = most_frequent_dst_entity(train_table, test_table, val_table, task)
-    elif args.method == "random":
-        preds = random_dst_entity(train_table, test_table, val_table, task)
-
-    print(f"Run: {run}")
-    print(f"Method: {args.method}")
-    print(task.evaluate(preds))
+eval_name_list = ["past_visit", "global_popularity"]
+for name in eval_name_list:
+    train_metrics = evaluate(train_table, train_table, name=name)
+    val_metrics = evaluate(train_table, val_table, name=name)
+    test_metrics = evaluate(trainval_table, test_table, name=name)
+    print(f"{name}:")
+    print(f"Train: {train_metrics}")
+    print(f"Val: {val_metrics}")
+    print(f"Test: {test_metrics}")
