41bd208b3428f50fcb471309beae467c78a3443d
Weihua Hu <weihua916@gmail.com>
2024-04-05 00:55:52 +0000

    update

--- relbench/datasets/trial.py
+++ relbench/datasets/trial.py
@@ -17,8 +17,8 @@ from relbench.utils import unzip_processor
 
 class TrialDataset(RelBenchDataset):
     name = "rel-trial"
-    # 2 years gap
-    val_timestamp = pd.Timestamp("2019-01-01")
+    # 1 year gap
+    val_timestamp = pd.Timestamp("2020-01-01")
     test_timestamp = pd.Timestamp("2021-01-01")
     max_eval_time_frames = 1
     task_cls_list = [
