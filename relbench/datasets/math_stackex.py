e828bb745113f6b476f7fbeede1f7f1bb2c97512
Alejandro Dobles <alejandro.d.96@gmail.com>
2024-05-10 18:09:40 -0700

    Fix parsing error in `datasets/math_stackex.py` (#158)

--- relbench/datasets/math_stackex.py
+++ relbench/datasets/math_stackex.py
@@ -53,7 +53,9 @@ class MathStackExDataset(RelBenchDataset):
         path = os.path.join(path, "math-stackex-temp")
         print("Loading data from:", path)
         users = pd.read_csv(os.path.join(path, "Users.csv"))
-        comments = pd.read_csv(os.path.join(path, "Comments.csv"), low_memory=False)
+        comments = pd.read_csv(
+            os.path.join(path, "Comments.csv"), low_memory=False, lineterminator="\n"
+        )
         posts = pd.read_csv(os.path.join(path, "Posts.csv"))
 
         votes = pd.read_csv(os.path.join(path, "Votes.csv"))
