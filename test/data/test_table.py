cd872739929e6b52965d475371dda76d207d6a70
yiweny <yyuanlisette@gmail.com>
2024-05-28 00:10:23 +0000

    fize upto function on data with timezone information and add test cases

--- test/data/test_table.py
+++ test/data/test_table.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
 from relbench.data import Table
-
+from datetime import datetime
+import pytz
 
 def test_table():
     table = Table(df=pd.DataFrame(), fkey_col_to_pkey_table={})
     assert len(table) == 0
+
+def test_table_timezone():
+    table = Table(df=pd.DataFrame({'timestamp': [datetime(2021, 1, 1, tzinfo=pytz.utc) for _ in range(5)]}), fkey_col_to_pkey_table={})
+
+    table.upto(pd.Timestamp('2022-01-01'))
+
+    table = Table(df=pd.DataFrame({'timestamp': [datetime(2021, 1, 1, tzinfo=None) for _ in range(5)]}), fkey_col_to_pkey_table={})
+
+    table.upto(pd.Timestamp('2022-01-01', tzinfo=pytz.utc))
+
+    table = Table(df=pd.DataFrame({'timestamp': [datetime(2021, 1, 1, tzinfo=pytz.utc) for _ in range(5)]}), fkey_col_to_pkey_table={})
+    table.upto(pd.Timestamp('2022-01-01', tzinfo=pytz.timezone('America/Los_Angeles')))
