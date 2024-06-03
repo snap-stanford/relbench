2d486694b70bc40c559eaff59f88b4bc7b2715cb
Xinwei <xinwei@kumo.ai>
2024-06-03 05:57:50 +0000

    fix

--- relbench/external/utils.py
+++ relbench/external/utils.py
@@ -1,8 +1,7 @@
-from typing import Dict
+from typing import Any, Dict
 
 import numpy as np
 import pandas as pd
-from torch_frame import stype
 
 from relbench.data import Table
 
@@ -17,7 +16,7 @@ def to_unix_time(ser: pd.Series) -> np.ndarray:
     return unix_time
 
 
-def remove_pkey_fkey(col_to_stype: Dict[str, stype], table: Table) -> dict:
+def remove_pkey_fkey(col_to_stype: Dict[str, Any], table: Table) -> dict:
     r"""Remove pkey, fkey columns since they will not be used as input feature."""
     if table.pkey_col is not None:
         if table.pkey_col in col_to_stype:
