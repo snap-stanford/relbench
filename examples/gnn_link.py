90b2ccabad5e2585fc842e6506367dd8a2e757fb
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-06-04 11:36:19 -0700

    fix count_accum bug

--- examples/gnn_link.py
+++ examples/gnn_link.py
@@ -183,7 +183,7 @@ def train() -> float:
         if steps > args.max_steps_per_epoch:
             break
 
-    return loss_accum / count_accum
+    return loss_accum / (count_accum + 1e-10)  # sometimes count_accum is 0
 
 
 @torch.no_grad()
