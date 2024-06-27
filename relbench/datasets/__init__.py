e3453a625743234c2aa3646cc9eb760908b5b915
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-06-27 14:43:10 -0700

    all datasets and tasks registered

--- relbench/datasets/__init__.py
+++ relbench/datasets/__init__.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
 from ..data import Dataset
-from . import amazon
+from . import amazon, avito, event, f1, hm, stack, trial
 
 dataset_registry = {}
 
@@ -27,3 +27,9 @@ def get_dataset(name: str) -> Dataset:
 
 
 register_dataset("rel-amazon", amazon.AmazonDataset)
+register_dataset("rel-avito", avito.AvitoDataset)
+register_dataset("rel-event", event.EventDataset)
+register_dataset("rel-f1", f1.F1Dataset)
+register_dataset("rel-hm", hm.HMDataset)
+register_dataset("rel-stack", stack.StackDataset)
+register_dataset("rel-trial", trial.TrialDataset)
--- relbench/tasks/__init__.py
+++ relbench/tasks/__init__.py
@@ -2,7 +2,8 @@ from collections import defaultdict
 from functools import lru_cache
 
 from ..data import BaseTask
-from . import amazon
+from ..datasets import get_dataset
+from . import amazon, avito, event, f1, hm, stack, trial
 
 task_registry = defaultdict(dict)
 
@@ -36,3 +37,34 @@ register_task("rel-amazon", "item-ltv", amazon.ItemLTVTask)
 register_task("rel-amazon", "user-item-purchase", amazon.UserItemPurchaseTask)
 register_task("rel-amazon", "user-item-rate", amazon.UserItemRateTask)
 register_task("rel-amazon", "user-item-review", amazon.UserItemReviewTask)
+
+register_task("rel-avito", "ads-click", avito.AdsClickTask)
+register_task("rel-avito", "user-visits", avito.UserVisitsTask)
+register_task("rel-avito", "user-clicks", avito.UserClicksTask)
+register_task("rel-avito", "user-ad-visit", avito.UserAdVisitTask)
+
+register_task("rel-event", "user-attendance", event.UserAttendanceTask)
+register_task("rel-event", "user-repeat", event.UserRepeatTask)
+register_task("rel-event", "user-ignore", event.UserIgnoreTask)
+
+register_task("rel-f1", "driver-position", f1.DriverPositionTask)
+register_task("rel-f1", "driver-dnf", f1.DriverDNFTask)
+register_task("rel-f1", "driver-top3", f1.DriverTop3Task)
+register_task("rel-f1", "driver-constructor-result", f1.DriverConstructorResultTask)
+
+register_task("rel-hm", "user-item-purchase", hm.UserItemPurchaseTask)
+register_task("rel-hm", "user-churn", hm.UserChurnTask)
+register_task("rel-hm", "item-sales", hm.ItemSalesTask)
+
+register_task("rel-stack", "user-engagement", stack.UserEngagementTask)
+register_task("rel-stack", "post-votes", stack.PostVotesTask)
+register_task("rel-stack", "user-badge", stack.UserBadgeTask)
+register_task("rel-stack", "user-post-comment", stack.UserPostCommentTask)
+register_task("rel-stack", "post-post-related", stack.PostPostRelatedTask)
+
+register_task("rel-trial", "study-outcome", trial.StudyOutcomeTask)
+register_task("rel-trial", "study-adverse", trial.StudyAdverseTask)
+register_task("rel-trial", "study-withdrawal", trial.StudyWithdrawalTask)
+register_task("rel-trial", "site-success", trial.SiteSuccessTask)
+register_task("rel-trial", "condition-sponsor-run", trial.ConditionSponsorRunTask)
+register_task("rel-trial", "site-sponsor-run", trial.SiteSponsorRunTask)
