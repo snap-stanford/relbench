00bc9a60c35b5cfa4a3355e93062bf2d7c011b42
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-07-01 00:05:07 -0700

    add avito database and tasks

--- relbench/data/dataset.py
+++ relbench/data/dataset.py
@@ -11,7 +11,6 @@ import numpy as np
 import pandas as pd
 import pooch
 
-from relbench import DOWNLOAD_REGISTRY
 from relbench.data.database import Database
 from relbench.utils import unzip_processor
 
