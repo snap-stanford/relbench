7456a6b35b606be0ce7f4c8e413f5c58085b80f5
yiweny <yyuanlisette@gmail.com>
2024-07-10 06:53:03 +0000

    make some functions accessible on library level

---
+++ relbench/modeling/__init__.py
@@ -0,0 +1,7 @@
+from .utils import get_stype_proposal
+from .graph import get_node_train_table_input, make_pkey_fkey_graph
+__all__ = [
+    'get_node_train_table_input',
+    'get_stype_proposal',
+    'make_pkey_fkey_graph',
+]
