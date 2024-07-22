01c8dfcdd596d4bdfa8795e6ebb503459127d30b
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-07-22 15:08:01 -0700

    fix

--- relbench/modeling/utils.py
+++ relbench/modeling/utils.py
@@ -11,7 +11,7 @@ from relbench.base import Database, Table
 def to_unix_time(ser: pd.Series) -> np.ndarray:
     r"""Converts a :class:`pandas.Timestamp` series to UNIX timestamp (in seconds)."""
     assert ser.dtype in [np.dtype("datetime64[s]"), np.dtype("datetime64[ns]")]
-    unix_time = ser.astype(int).values
+    unix_time = ser.astype("int64").values
     if ser.dtype == np.dtype("datetime64[ns]"):
         unix_time //= 10**9
     return unix_time
