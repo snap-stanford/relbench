c195a5ebc00a6b2500a5a1d9e2321192cc741ad8
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-07-25 14:50:48 -0700

    update

--- relbench/datasets/event.py
+++ relbench/datasets/event.py
@@ -54,18 +54,18 @@ class EventDataset(Dataset):
         users_df = pd.read_csv(users, dtype={"user_id": int}, parse_dates=["joinedAt"])
         users_df["birthyear"] = pd.to_numeric(users_df["birthyear"], errors="coerce")
         users_df["joinedAt"] = pd.to_datetime(
-            users_df["joinedAt"], errors="coerce", format="%Y-%m-%d %H:%M:%S.%f%z"
+            users_df["joinedAt"], errors="coerce", format="mixed"
         ).dt.tz_localize(None)
 
         events_df = pd.read_csv(events)
         events_df["start_time"] = pd.to_datetime(
-            events_df["start_time"], errors="coerce", format="%Y-%m-%d %H:%M:%S.%f%z"
+            events_df["start_time"], errors="coerce", format="mixed"
         ).dt.tz_localize(None)
 
         train = os.path.join(path, "train.csv")
         event_interest_df = pd.read_csv(train)
         event_interest_df["timestamp"] = pd.to_datetime(
-            event_interest_df["timestamp"], format="%Y-%m-%d %H:%M:%S.%f%z"
+            event_interest_df["timestamp"], format="mixed"
         ).dt.tz_localize(None)
 
         if not os.path.exists(os.path.join(path, "user_friends_flattened.csv")):
