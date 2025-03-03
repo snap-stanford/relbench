72f4c4a7d38ad2f731c06849fec554983a5646d3
fengyuli <fengyuli2002@gmail.com>
2025-03-03 09:41:02 -0800

    Format

--- relbench/datasets/arxiv.py
+++ relbench/datasets/arxiv.py
@@ -11,8 +11,10 @@ class ArxivDataset(Dataset):
     def make_db(self) -> Database:
         r"""Process the raw files into a database."""
 
-        url = ("https://www.dropbox.com/scl/fi/tjj6r1fqikt4j0rz4qomu/db.zip?rlkey=1ykfkp8pj3hu6n4utz8g9dkx2&st"
-               "=azmm56dc&dl=1")
+        url = (
+            "https://www.dropbox.com/scl/fi/tjj6r1fqikt4j0rz4qomu/db.zip?rlkey=1ykfkp8pj3hu6n4utz8g9dkx2&st"
+            "=azmm56dc&dl=1"
+        )
 
         path = pooch.retrieve(
             url,
@@ -34,45 +36,65 @@ class ArxivDataset(Dataset):
         paperAuthors = pd.read_csv(os.path.join(path, "6Paper_Author.csv"))
 
         # Convert date column to pd.Timestamp
-        papers["Submission_Date"] = pd.to_datetime(papers["Submission_Date"], format='%Y%m%d')
-        citations["Submission_Date"] = pd.to_datetime(citations["Submission_Date"], format='%Y%m%d')
-        paperAuthors["Submission_Date"] = pd.to_datetime(paperAuthors["Submission_Date"], format='%Y%m%d')
+        papers["Submission_Date"] = pd.to_datetime(
+            papers["Submission_Date"], format="%Y%m%d"
+        )
+        citations["Submission_Date"] = pd.to_datetime(
+            citations["Submission_Date"], format="%Y%m%d"
+        )
+        paperAuthors["Submission_Date"] = pd.to_datetime(
+            paperAuthors["Submission_Date"], format="%Y%m%d"
+        )
 
         # add time column to other tables
-        paperCategories = paperCategories.merge(papers[["Paper_ID", "Submission_Date"]], on="Paper_ID", how="left")
+        paperCategories = paperCategories.merge(
+            papers[["Paper_ID", "Submission_Date"]], on="Paper_ID", how="left"
+        )
 
         # collect all tables in the database as relbench.base.Table objects.
-
-        tables = {"papers": Table(
-            df=pd.DataFrame(papers),
-            fkey_col_to_pkey_table={},
-            pkey_col="Paper_ID",
-            time_col="Submission_Date",
-        ), "categories": Table(
-            df=pd.DataFrame(categories),
-            fkey_col_to_pkey_table={},
-            pkey_col="Category_ID",
-            time_col=None,
-        ), "citations": Table(
-            df=pd.DataFrame(citations),
-            fkey_col_to_pkey_table={"Paper_ID": "papers", "References_Paper_ID": "papers"},
-            pkey_col=None,
-            time_col="Submission_Date",
-        ), "paperCategories": Table(
-            df=pd.DataFrame(paperCategories),
-            fkey_col_to_pkey_table={"Paper_ID": "papers", "Category_ID": "categories"},
-            pkey_col=None,
-            time_col="Submission_Date",
-        ), "authors": Table(
-            df=pd.DataFrame(authors),
-            fkey_col_to_pkey_table={},
-            pkey_col="Author_ID",
-            time_col=None,
-        ), "paperAuthors": Table(
-            df=pd.DataFrame(paperAuthors),
-            fkey_col_to_pkey_table={"Paper_ID": "papers", "Author_ID": "authors"},
-            pkey_col=None,
-            time_col="Submission_Date",
-        )}
+        tables = {
+            "papers": Table(
+                df=pd.DataFrame(papers),
+                fkey_col_to_pkey_table={},
+                pkey_col="Paper_ID",
+                time_col="Submission_Date",
+            ),
+            "categories": Table(
+                df=pd.DataFrame(categories),
+                fkey_col_to_pkey_table={},
+                pkey_col="Category_ID",
+                time_col=None,
+            ),
+            "citations": Table(
+                df=pd.DataFrame(citations),
+                fkey_col_to_pkey_table={
+                    "Paper_ID": "papers",
+                    "References_Paper_ID": "papers",
+                },
+                pkey_col=None,
+                time_col="Submission_Date",
+            ),
+            "paperCategories": Table(
+                df=pd.DataFrame(paperCategories),
+                fkey_col_to_pkey_table={
+                    "Paper_ID": "papers",
+                    "Category_ID": "categories",
+                },
+                pkey_col=None,
+                time_col="Submission_Date",
+            ),
+            "authors": Table(
+                df=pd.DataFrame(authors),
+                fkey_col_to_pkey_table={},
+                pkey_col="Author_ID",
+                time_col=None,
+            ),
+            "paperAuthors": Table(
+                df=pd.DataFrame(paperAuthors),
+                fkey_col_to_pkey_table={"Paper_ID": "papers", "Author_ID": "authors"},
+                pkey_col=None,
+                time_col="Submission_Date",
+            ),
+        }
 
         return Database(tables)
