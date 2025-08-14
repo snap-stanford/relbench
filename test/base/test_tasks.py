3a2422ecbe49f6b00004986b1719dc65106984f7
ValterH <valter.hudovernik@gmail.com>
2025-08-13 19:40:56 -0700

    update autocomplete task tests

--- test/base/test_tasks.py
+++ test/base/test_tasks.py
@@ -15,8 +15,13 @@ def test_autocomplete_task():
         entity_table="review",
         target_col="rating",
         cache_dir=None,
-        remove_columns=[],
+        remove_columns=[("review", "review")],
     )
+    # ensure columns are removed correctly
+    for table, column in task.dataset.remove_columns:
+        assert column not in task.dataset.get_db().table_dict[table].df.columns
+
+    # ensure all entities are present
     db = dataset.get_db(upto_test_timestamp=False)
     pks = db.table_dict["review"].df.get("primary_key")
     train_table = task.get_table("train")
@@ -28,21 +33,14 @@ def test_autocomplete_task():
     )
     assert task_table_full.primary_key.isin(pks).all()
 
-    # ensure get_task can be called multiple times on the same database
-    dataset = get_dataset("rel-f1")
-    db = dataset.get_db()
-    results_columns = db.table_dict["results"].df.columns.tolist()
-
-    task = get_task("rel-f1", "results-position")
-    table = task.get_table("train")
-
-    # ensure columns are removed correctly
-    for table, column in task.dataset.remove_columns:
-        assert column not in task.dataset.get_db().table_dict[table].df.columns
-
-    task = get_task("rel-f1", "qualifying-position")
-    # ensure the results table contains all the correct columns
-    assert (
-        task.dataset.get_db().table_dict["results"].df.columns.tolist()
-        == results_columns
+    # ensure the task can be constructed multiple times on the same database
+    task = AutoCompleteTask(
+        dataset=dataset,
+        task_type=TaskType.MULTICLASS_CLASSIFICATION,
+        entity_table="review",
+        target_col="rating",
+        cache_dir=None,
+        remove_columns=[],
     )
+    # ensure the review column is present as it was not removed this time
+    assert "review" in dataset.get_db().table_dict["review"].df.columns
